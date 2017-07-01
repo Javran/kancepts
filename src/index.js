8ac2f01246c3aed16922d533fdd40c043749c86b
Javran Cheng <Javran.c@gmail.com>
2017-06-30 23:47:50 -0400

    deps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { join } from 'path-extra'
+import { Provider } from 'react-redux'
 
 import 'font-awesome/css/font-awesome.css'
 import './assets/index.css'
@@ -8,13 +9,17 @@ import './assets/index.css'
 import { ExpedRecommender } from './ui'
 import { register } from './registerServiceWorker'
 
+import { store } from './store'
+
 ReactDOM.render(
-  <div className="root">
-    <link
-      rel="stylesheet"
-      href={join(__dirname,'..','assets','index.css')}
-    />
-    <ExpedRecommender />
-  </div>,
+  <Provider store={store}>
+    <div className="root">
+      <link
+        rel="stylesheet"
+        href={join(__dirname,'..','assets','index.css')}
+      />
+      <ExpedRecommender />
+    </div>
+  </Provider>,
   document.getElementById('root'))
 register()
