fd823a1d695b7b98bc9675476a922335fd60bf23
Javran Cheng <Javran.c@gmail.com>
2017-07-09 23:58:01 -0400

    panels & exped controls

--- src/ui/dlc-lab/index.js
+++ src/ui/dlc-lab/index.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react'
+import {
+  Panel,
+  FormGroup,
+  Checkbox,
+} from 'react-bootstrap'
+import Slider from 'rc-slider'
 
 class DlcLab extends Component {
   render() {
     return (
-      <div>DLC Lab placeholder</div>
+      <div style={{display: 'flex'}}>
+        <Panel
+          className="dlc-lab-control-panel"
+          style={{
+            width: '60%',
+            marginRight: 10,
+          }}
+          header="Expedition"
+        >
+          <FormGroup>
+            <Checkbox inline>Include Kinu K2</Checkbox>
+          </FormGroup>
+          <div style={{display: 'flex'}}>
+            <div>
+              Great Success Rate: 0%
+            </div>
+            <Slider
+              style={{flex: 1, marginLeft: '1.2em', marginRight: '1em'}}
+              marks={{0: '0%', 50: '50%', 100: '100%'}} />
+          </div>
+          <div>
+            Equipment list placeholder
+          </div>
+          <div>
+            TODO panel for adding new equipments
+          </div>
+        </Panel>
+        <Panel
+          className="dlc-lab-control-panel"
+          style={{flex: 1}}
+          header="Results"
+        >
+          exped result placeholder
+        </Panel>
+      </div>
     )
   }
 }
