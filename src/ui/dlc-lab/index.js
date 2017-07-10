08251fdfb311caa0bf5a449a686287a1746f30ca
Javran Cheng <Javran.c@gmail.com>
2017-07-10 00:24:53 -0400

    "add equipment" panel

--- src/ui/dlc-lab/index.js
+++ src/ui/dlc-lab/index.js
@@ -3,8 +3,16 @@ import {
   Panel,
   FormGroup,
   Checkbox,
+  Radio,
+  Button,
+  ButtonGroup,
+  DropdownButton,
+  FormControl,
+  MenuItem,
 } from 'react-bootstrap'
 import Slider from 'rc-slider'
+
+import { enumFromTo } from '../../utils'
 
 class DlcLab extends Component {
   render() {
@@ -32,9 +40,39 @@ class DlcLab extends Component {
           <div>
             Equipment list placeholder
           </div>
-          <div>
-            TODO panel for adding new equipments
-          </div>
+          <Panel
+            className="dlc-lab-control-panel"
+            style={{marginBottom: 5}}
+          >
+            <DropdownButton
+              style={{marginTop: 5}}
+              title="大発動艇"
+              id="dlc-lab-new-item-type">
+              <MenuItem>大発動艇</MenuItem>
+              <MenuItem>特大発動艇</MenuItem>
+              <MenuItem>大発動艇(八九式中戦車&陸戦隊)</MenuItem>
+              <MenuItem>特型内火艇</MenuItem>
+            </DropdownButton>
+            <div style={{
+              display: 'flex',
+              alignItems: 'center',
+              marginTop: 5,
+            }}>
+              <div style={{width: '40%', marginRight: 8}}>
+                <ButtonGroup justified>
+                  <DropdownButton id="dlc-lab-new-item-level" title="star">
+                    {
+                      enumFromTo(0,10).map(x => (
+                        <MenuItem key={x}>{x}</MenuItem>
+                      ))
+                    }
+                  </DropdownButton>
+                </ButtonGroup>
+              </div>
+              <FormControl style={{width: '40%', marginRight: 8}} type="number" />
+              <Button style={{flex: 1}}>Add</Button>
+            </div>
+          </Panel>
         </Panel>
         <Panel
           className="dlc-lab-control-panel"
