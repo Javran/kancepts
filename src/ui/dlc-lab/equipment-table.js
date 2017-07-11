b85273c5d28057c1e9f832574cfe960003c9072c
Javran Cheng <Javran.c@gmail.com>
2017-07-11 02:54:55 -0400

    equipment info to table

--- src/ui/dlc-lab/equipment-table.js
+++ src/ui/dlc-lab/equipment-table.js
@@ -1,8 +1,12 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
+import {
+  Table,
+} from 'react-bootstrap'
 import { PTyp } from '../../ptyp'
 import { dlcLabSelector } from '../../selectors'
+import { enumFromTo } from '../../utils'
 
 class EquipmentTableImpl extends Component {
   static propTypes = {
@@ -17,9 +21,53 @@ class EquipmentTableImpl extends Component {
   render() {
     const {equipments, style} = this.props
     return (
-      <div style={style}>
-        Raw: {JSON.stringify(equipments)}
-      </div>
+      <Table
+        style={style}
+        bordered condensed>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Level</th>
+            <th>Count</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            _.flatMap(
+              [
+                ['大発動艇', 68],
+                ['特大発動艇', 193],
+                ['大発動艇(八九式中戦車&陸戦隊)', 166],
+                ['特型内火艇', 167],
+              ],
+              ([name,masterId]) => {
+                const equipment = equipments[masterId]
+                if (typeof equipment === 'undefined')
+                  return []
+                const partialRows = _.flatMap(
+                  enumFromTo(0,10),
+                  (level,ind) => {
+                    const count = equipment[level]
+                    if (! _.isInteger(count))
+                      return []
+                    const key = `${masterId}-${level}`
+                    return [rowSpan => (
+                      <tr key={key}>
+                        {
+                          ind === 0 &&
+                          (<td rowSpan={rowSpan}>{name}</td>)
+                        }
+                        <td>{level}</td>
+                        <td>{count}</td>
+                      </tr>
+                    )]
+                  })
+                return partialRows.map(f =>
+                  f(partialRows.length))
+              })
+          }
+        </tbody>
+      </Table>
     )
   }
 }
