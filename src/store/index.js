454ffeb9eb2e29da70bd0652ae1fb98e264cd6de
Javran Cheng <Javran.c@gmail.com>
2017-08-25 23:37:59 -0400

    shiplist import, format draft

--- src/store/index.js
+++ src/store/index.js
@@ -9,4 +9,35 @@ const store = createStore(reducer, preloadedState)
 
 window.getStore = store.getState
 
+/*
+   TODO: shiplist import format:
+
+   '?sl=<encoded>'
+
+   <encoded> should be an comma separated list encoded by encodeURIComponent
+
+   - every element is a number indicating ship id.
+   - an optional prefix 'R' / 'r' can be prefixed to the ship id indicating a married ship
+
+   - example:
+
+     - r318: Ryuuhou Kai (married)
+     - r185: Ryuuhou (married)
+     - r184: Taigei (married)
+     - 393: Ark Royal (not married)
+
+     putting together:
+
+     - r318,r185,r184,393
+     - r318%2Cr185%2Cr184%2C393 (encoded by encodeURIComponent)
+     - so the final search string should be: '?sl=r318%2Cr185%2Cr184%2C393'
+
+ */
+
+setTimeout(() => {
+  const searchString = window.location.search
+  console.log(window.location)
+  console.log(searchString)
+})
+
 export { store }
