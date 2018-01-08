8476ef642c489e48531b5fc918ea885bbdb83450
Javran Cheng <Javran.c@gmail.com>
2018-01-07 23:30:00 -0500

    need somewhere to put maintenance notes

--- src/index.js
+++ src/index.js
@@ -11,6 +11,26 @@ import { register } from './registerServiceWorker'
 
 import { store } from './store'
 
+/*
+
+   TODO: need to keep a maintenance node somewhere.
+
+   - when api_start2 gets updated:
+
+       - use `misc/normalize-master-data.js <HAR> api_start2.json` to extra master data
+
+   - when new remodel is implemented:
+
+       - use `misc/generate-default-ship-list.js`
+
+   - when new exped is implemented
+
+      - update `assets/exped-info.json`
+      - (TODO) we'll switch to use master data some day.
+      - update `src/exped-info/fleet-compo.js`
+
+*/
+
 ReactDOM.render(
   (
     <div className="root">
