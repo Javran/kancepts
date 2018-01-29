cefe9c6c63099e292040191a467aaf77851e387b
Javran Cheng <Javran.c@gmail.com>
2018-01-28 23:34:19 -0500

    remove unused

--- src/ui/exped-table/index.js
+++ src/ui/exped-table/index.js
@@ -5,47 +5,25 @@ import { connect } from 'react-redux'
 import {
   ListGroup, ListGroupItem,
 } from 'react-bootstrap'
-import FontAwesome from 'react-fontawesome'
 
 import { mapDispatchToProps } from '../../store/reducer/exped-configs'
 import {
   expedInfoViewListSelector,
 } from '../../selectors'
 import {
-  resourceProperties,
   allExpedIdList,
 } from '../../exped-info'
 
-import { ItemIcon } from '../item-icon'
 import { ExpedRow } from './exped-row'
 import { PTyp } from './../../ptyp'
 
 import { TableControl } from './table-control'
-
-const defineHeader = (key, content, style) => ({key, content, style})
 
 class ExpedTableImpl extends Component {
   static propTypes = {
     expedInfoViewList: PTyp.array.isRequired,
     modifyExpedConfig: PTyp.func.isRequired,
   }
-
-  static headers = [
-    defineHeader(
-      'id', '#', {width: '2em'}),
-    defineHeader(
-      'name', 'Name', {}),
-    defineHeader(
-      'time', <FontAwesome name="clock-o" />, {width: '3.2em'}),
-    ...resourceProperties.map(rp =>
-      defineHeader(
-        rp, <ItemIcon name={rp} style={{height: '1em'}} /> , {width: '3.4em'})),
-    defineHeader('item-1', 'Item 1', {width: '4em'}),
-    defineHeader('item-2', 'Item 2', {width: '4em'}),
-    defineHeader('mod', 'Modifier', {width: '8em'}),
-    defineHeader('cost', 'Cost', {width: '8em'}),
-    defineHeader('control', '...', {width: '4em'}),
-  ]
 
   constructor(props) {
     super(props)
