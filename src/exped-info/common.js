ef6e1a6b67d1dc74986ad7979322dd7dc03eac2a
Javran Cheng <Javran.c@gmail.com>
2018-02-22 22:00:43 -0500

    exped id groupping

--- src/exped-info/common.js
+++ src/exped-info/common.js
@@ -7,6 +7,21 @@ import { $missions } from '../master-data'
 const allExpedIdList =
   Object.keys($missions).map(Number).sort((x,y) => x-y).filter(x => x < 300)
 
+// Array of [worldId, Array of raw mission], sorted.
+// TODO: replace some uses of allExpedIdList by grouppedExpedIds
+const grouppedExpedIds =
+  _.sortBy(
+    _.toPairs(
+      _.groupBy(
+        _.values($missions).filter(x => x.api_maparea_id < 30),
+        'api_maparea_id'
+      )
+    ).map(([k,ms]) =>
+      [Number(k), _.sortBy(ms, 'api_id')]
+    ),
+    ([k,_v]) => k
+  )
+
 const resourceProperties = ['fuel', 'ammo', 'steel', 'bauxite']
 
 const emptyResource = _.fromPairs(
@@ -19,6 +34,7 @@ const liftBinaryToResource = binFunc => (resource1, resource2) =>
 
 export {
   allExpedIdList,
+  grouppedExpedIds,
   resourceProperties,
   emptyResource,
   liftBinaryToResource,
